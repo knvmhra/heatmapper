09f66115c975d601493578810ccc6b29c3ef0549
Personal Work <kanav@themehra.com>
2025-02-16 18:15:15 -0600

    delete test file

--- test.py
+++
@@ -1,7 +0,0 @@
-from Foundation import NSBundle
-import objc
-
-bundle_path = 'System/Library/Frameworks/CoreWLAN.framework'
-objc.loadBundle('CoreWLAN',
-                bundle_path = bundle_path,
-                module_globals = globals())
